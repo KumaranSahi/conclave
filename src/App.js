f272825810e61abbc4c048e5577c526b8b213519
Muthu Kumaran <61352185+KumaranSahi@users.noreply.github.com>
2021-05-01 12:00:50 +0530

    feature- added signup page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import './App.css';
 import {NavBar} from './Components'
+import {Switch,Route} from 'react-router-dom'
+import SignUp from './Pages/SignUp/SignUp'
+import {ToastContainer} from 'react-toastify'
 
 function App() {
   return (
@@ -7,6 +10,12 @@ function App() {
       <header>
         <NavBar/>
       </header>
+      <main className="main-container">
+        <Switch>
+          <Route path="/sign-up" component={SignUp}/>
+        </Switch>
+      </main>
+      <ToastContainer/>
     </div>
   );
 }
